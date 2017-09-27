2101cb66848660173d9deddc41866c7acfbbd89f
richbid <call13570082878@qq.com>
2017-09-27 10:10:05 +0800

    Add files via upload

---
+++ cs/argument1/Program.cs
@@ -0,0 +1,11 @@
+﻿//TEMPlATE FOR MS CS2010
+namespace argument1
+{
+    static class Program
+    {
+        static int Main(string[] argv)
+        {
+            return 0;
+        }
+    }
+}
---
+++ cs/diagram1/Program.cs
@@ -0,0 +1,11 @@
+﻿//TEMPlATE FOR MS CS2010
+namespace diagram1
+{
+    static class Program
+    {
+        static int Main(string[] argv)
+        {
+            return 0;
+        }
+    }
+}
