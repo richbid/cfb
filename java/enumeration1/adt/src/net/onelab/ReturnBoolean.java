12ce3ed09276504da66f104efb91fd30039986d0
richbid <call13570082878@qq.com>
2017-09-27 14:28:32 +0800

    Add files via upload

---
+++ java/enumeration1/adt/src/net/onelab/ReturnBoolean.java
@@ -0,0 +1,11 @@
+package net.onelab;
+
+public class ReturnBoolean {
+	public boolean value = false;
+	public ReturnBoolean() {
+		
+	}
+	public ReturnBoolean(boolean v) {
+		value = v;
+	}	
+}
---
+++ java/enumeration1/adt/src/net/onelab/ReturnInt32.java
@@ -0,0 +1,11 @@
+package net.onelab;
+
+public class ReturnInt32 {
+	public int value = 0;
+	public ReturnInt32() {
+		
+	}
+	public ReturnInt32(int v) {
+		value = v;
+	}	
+}
