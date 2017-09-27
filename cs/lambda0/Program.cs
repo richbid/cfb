f2c66df030a1409051471c63df26d9701e65a512
richbid <call13570082878@qq.com>
2017-09-27 10:26:01 +0800

    Add files via upload

---
+++ cs/lambda0/Program.cs
@@ -0,0 +1,11 @@
+﻿//TEMPlATE FOR MS CS2010
+namespace lambda0
+{
+    static class Program
+    {
+        static int Main(string[] argv)
+        {
+            return 0;
+        }
+    }
+}
---
+++ cs/module0/Program.cs
@@ -0,0 +1,11 @@
+﻿//TEMPlATE FOR MS CS2010
+namespace module0
+{
+    static class Program
+    {
+        static int Main(string[] argv)
+        {
+            return 0;
+        }
+    }
+}
