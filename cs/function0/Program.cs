4229ddeb41f0ce9d04bd8b6f5835a5670ccbb9b2
richbid <call13570082878@qq.com>
2017-09-27 10:18:13 +0800

    Add files via upload

---
+++ cs/function0/Program.cs
@@ -0,0 +1,11 @@
+﻿//TEMPlATE FOR MS CS2010
+namespace function0
+{
+    static class Program
+    {
+        static int Main(string[] argv)
+        {
+            return 0;
+        }
+    }
+}
---
+++ cs/function1/Program.cs
@@ -0,0 +1,11 @@
+﻿//TEMPlATE FOR MS CS2010
+namespace function1
+{
+    static class Program
+    {
+        static int Main(string[] argv)
+        {
+            return 0;
+        }
+    }
+}
