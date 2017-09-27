1827226b2cb303c57c0fc2e66841a306323c63c1
richbid <call13570082878@qq.com>
2017-09-27 15:22:31 +0800

    Add files via upload

---
+++ java/scopes1/adt/src/net/onelab/ReturnDatetime.java
@@ -0,0 +1,11 @@
+package net.onelab;
+
+public class ReturnDatetime {
+	public java.util.GregorianCalendar value = new java.util.GregorianCalendar();
+	public ReturnDatetime() {
+		
+	}
+	public ReturnDatetime(java.util.GregorianCalendar v) {
+		value = v;
+	}	
+}
